77be1c6d9947566edaa0e93c28e0805207549d5f
Firesun4 <66979283+Firesun4@users.noreply.github.com>
2022-09-01 16:53:26 -0500

    Create EncoderAuto.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderAuto.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous
+public class EncodeAuto extends LinearOpMode{
+    private DcMotorEx leftFront;
+    private DcMotorEx leftBack;
+    private DcMotorEx rightFront;
+    private DcMotorEx rightBack;
+
+    private double leftFrontPos;
+    private double leftBackPos;
+    private double rightFrontPos;
+    private double rightBackPos;
+
+    public void runOpMode(){
+        leftFront = (DcMotorEx) hardwareMap.dcMotor.get("leftFront");
+        leftBack = (DcMotorEx) hardwareMap.dcMotor.get("leftBack");
+        rightFront = (DcMotorEx) hardwareMap.dcMotor.get("rightFront");
+        rightBack = (DcMotorEx) hardwareMap.dcMotor.get("rightBack");
+
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftFront.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBack.setDirection(DcMotorEx.Direction.REVERSE);
+
+
+
+        leftFrontPos = 0.03;
+        leftBackPos = 0;
+        rightFrontPos = 0;
+        rightBackPos = 0.03  ;
+
+        waitForStart();
+    }
+
+    @Override
+    private void drive(double speed, int leftFrontTarget, int leftBackTarget, int rightFrontTarget, int rightBackTarget){
+        leftFrontPos += leftFrontTarget;
+        leftBackPos += leftBackTarget;
+        rightFrontPos += rightFrontTarget;
+        rightBackPos += rightBackTarget;
+
+        leftFront.setVelocity(10, AngleUnit.RADIANS);
+    }
+}
